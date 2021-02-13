a8737d3f9be336c30d7454a4f5b2861cc9ad03f7
Dustmix <43365173+Dustmix@users.noreply.github.com>
2021-02-13 22:15:42 +0100

    Added Autoupdates!*
    Still in beta-testing so they are not enabled, but you can enable them if you know where to look.

--- BingRDW.py
+++ BingRDW.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 import pickle
 import random
@@ -6,6 +8,7 @@ import shutil
 import pathlib
 import ctypes
 import sys
+from fun import update, first_time_here
 
 # What platform is this running from
 if sys.platform == "linux" or sys.platform == "linux2":
@@ -20,139 +23,28 @@ else:
     platform = "unknown"
     print("Warning! This platform is not supported, I don't even know what's the os.")
 
-# FUNctions ahah
-def first_time_here():
-    print("It seems you haven't used this thing yet...")
-    print("It's time for a first time setup! Don't worry it won't take long but you have to restart the script after the setup is done.")
-    step = 1
-
-    while step == 1:
-        print()
-        res = input("Do you prefer HD (1366) or FullHD (1920) images? ")
-        print(str(res))
-        if str(res) == "1920":
-            print("FullHD it is.")
-            res = 1920
-            step = 2
-            break
-        elif str(res) == "1366":
-            print("HD it is.")
-            step = 2
-            res = 1366
-            break
-        else:
-            print("Sorry i didn't understand that.")
-            print("You can type 1366 HD images.")
-            print("You can type 1920 FullHD images.")
-            print()
-            print("Let's retry.")
-
-    while step == 2:
-        print()
-        applyWall = input("Do you want me to apply the images after I download them? (Y/N) ")
-        if applyWall.casefold() == "y":
-            print("Ok. Remember that if I don't recognize the OS or the DE I won't apply it.")
-            applyWall = True
-            step = 3
-            break
-        elif applyWall.casefold() == "n":
-            print("Ok. I'm not going to apply any wallpaper.")
-            applyWall = False
-            step = 3
-            break
-        else:
-            print("I'm sorry but I need a yes or no answer here.")
-            print("Let's retry.")
-
-
-    while step == 3:
-        print()
-        RemoveImg = input("Do you want me to remove the images after I apply them? (Y/N) ")
-        if RemoveImg.casefold() == "y":
-            print("Yay, I get to use my new vaccumm cleaner!")
-            RemoveImg = True
-            step = 4
-            break
-        elif RemoveImg.casefold() == "n":
-            print("Oh, ok.")
-            RemoveImg = False
-            step = 4 
-            break
-        else:
-            print("I'm sorry but I need a yes or no answer here.")
-            print("Let's retry.")
-
-    while step == 4:
-        Desktop = 9999
-        fehoption = 9999
-        if platform == "linux" and applyWall == True:
-            print()
-            print("What DE are you using?")
-            print("1 - Gnome")
-            print("2 - Mate")
-            print("3 - Cinnamon")
-            print("0 - Other (use feh to apply wallpaper)")
-            Desktop = input("0-3? ")
-
-            if Desktop != 1 or 2 or 3 or 0:
-                print("Please choose an option.")
-                print()
-                print("Let's retry.")
-            else:
-                step = 5
-                break
-        else:
-            break
-    
-    while step == 5:
-        if Desktop == 0:
-            print()
-            print("Remember to install feh")
-            fehoption = input("Choose between --bg-fill, --bg-max, --bg-scale or --bg-tile. ")
-
-            if fehoption != "--bg-fill" or "--bg-max" or "--bg-scale" or "--bg-tile":
-                print("Please type --bg-max, --bg-max, --bg-scale or --bg-tile.")
-                print()
-                print("Let's retry.")
-            else:
-                break
-        else:
-            break
-    
-    old_index = 0
-    old_mkt_int = 0
-    
-    print("Thanks for your time, saving info...")
-    with open('brdwvars.pickle', 'wb') as f:
-        pickle.dump([old_index, old_mkt_int, res, applyWall, RemoveImg, Desktop, fehoption], f)
-    sys.exit()
-
 # Network test variable
 url = 'http://www.google.com'
 markets = ['zh-CN', 'en-US', 'ja-JP', 'en-AU', 'en-UK', 'de-DE', 'en-NZ', 'en-CA']
-network = False
 
 # Check Interwebz
-for attempt in range(10):
+while True: # I put it as an infinite loop because this program is ment to be run on a schedule everyday so it retrys until it find a connection
     try:
-        _ = requests.get(url, timeout = 10)
+        _ = requests.get(url, str(10))
     except requests.ConnectionError:
         print("PC isn't connected to the internet. Retrying...")
     else:
         print("PC is connected to the internet! Continuing...")
-        network = True
+        #update(os.path.basename(__file__))
         break
-
-if network == False:
-    print("Couldn't connect to the interwebs. Quitting...")
-    sys.exit()
 
 # Loading variables or running intial setup
 if os.path.isfile('brdwvars.pickle'):
     with open('brdwvars.pickle', 'rb') as f: 
         old_index, old_mkt_int, res, applyWall, RemoveImg, Desktop, fehoption = pickle.load(f)
 else:
-    first_time_here()
+    first_time_here(platform=platform)
+    sys.exit()
 
 # Setting index and mkt and duplicate index and mkt prevention
 while True:
@@ -214,6 +106,9 @@ if platform == "windows":
         print("Applying wallpaper...")
         print("Path: " + path)
         ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 3)
+        if RemoveImg == True: # Deleting image, if requested
+            os.remove(path)
+        sys.exit()
 
 elif platform == "linux":
     if applyWall == False:
@@ -233,13 +128,9 @@ elif platform == "linux":
         elif Desktop == 0:
             cmd = "feh " + fehoption + " " + path
             os.system(cmd)
+        if RemoveImg == True: # Deleting image, if requested
+            os.remove(path)
+        sys.exit()
 else:
     print("Setting wallpaper isn't supported on this platform. Quitting...")
-    sys.exit()
-
-# Deleting image, if requested
-if RemoveImg == True and applyWall == False:
-    os.remove(path)
-    sys.exit()
-else:
     sys.exit()
---
+++ fun.py
@@ -0,0 +1,188 @@
+import os
+import requests
+import pickle
+import requests
+import hashlib
+import shutil
+import sys
+
+# FUNction (I hate this)
+def first_time_here(platform):
+    print("It seems you haven't used this thing yet...")
+    print("It's time for a first time setup! Don't worry it won't take long but you have to restart the script after the setup is done.")
+    step = 1
+
+    while step == 1:
+        print()
+        res = input("Do you prefer HD (1366) or FullHD (1920) images? ")
+        print(str(res))
+        if str(res) == "1920":
+            print("FullHD it is.")
+            step = 2
+            break
+        elif str(res) == "1366":
+            print("HD it is.")
+            step = 2
+            break
+        else:
+            print("Sorry i didn't understand that.")
+            print("You can type 1366 HD images.")
+            print("You can type 1920 FullHD images.")
+            print()
+            print("Let's retry.")
+
+    while step == 2:
+        print()
+        applyWall = input("Do you want me to apply the images after I download them? (Y/N) ")
+        if applyWall.casefold() == "y":
+            print("Ok. Remember that if I don't recognize the OS or the DE I won't apply it.")
+            applyWall = True
+            step = 3
+            break
+        elif applyWall.casefold() == "n":
+            print("Ok. I'm not going to apply any wallpaper.")
+            applyWall = False
+            step = 3
+            break
+        else:
+            print("I'm sorry but I need a yes or no answer here.")
+            print("Let's retry.")
+
+
+    while step == 3:
+        print()
+        RemoveImg = input("Do you want me to remove the images after I apply them? (Y/N) ")
+        if RemoveImg.casefold() == "y":
+            print("Yay, I get to use my new vaccumm cleaner!")
+            RemoveImg = True
+            step = 4
+            break
+        elif RemoveImg.casefold() == "n":
+            print("Oh, ok.")
+            RemoveImg = False
+            step = 4 
+            break
+        else:
+            print("I'm sorry but I need a yes or no answer here.")
+            print("Let's retry.")
+
+    while step == 4:
+        Desktop = 9999
+        fehoption = 9999
+        if platform == "linux" and applyWall == True:
+            print()
+            print("What DE are you using?")
+            print("1 - Gnome")
+            print("2 - Mate")
+            print("3 - Cinnamon")
+            print("0 - Other (use feh to apply wallpaper)")
+            Desktop = input("0-3? ")
+
+            if Desktop != 1 or 2 or 3 or 0:
+                print("Please choose an option.")
+                print()
+                print("Let's retry.")
+            else:
+                step = 5
+                break
+        else:
+            break
+    
+    while step == 5:
+        if Desktop == 0:
+            print()
+            print("Remember to install feh")
+            fehoption = input("Choose between --bg-fill, --bg-max, --bg-scale or --bg-tile. ")
+
+            if fehoption != "--bg-fill" or "--bg-max" or "--bg-scale" or "--bg-tile":
+                print("Please type --bg-max, --bg-max, --bg-scale or --bg-tile.")
+                print()
+                print("Let's retry.")
+            else:
+                break
+        else:
+            break
+    
+    old_index = 0
+    old_mkt_int = 0
+    
+    print("Thanks for your time, saving info...")
+    with open('brdwvars.pickle', 'wb') as f:
+        pickle.dump([old_index, old_mkt_int, res, applyWall, RemoveImg, Desktop, fehoption], f)
+
+def update(fname):
+    # Download a new MD5
+    repo = "https://raw.githubusercontent.com/Dustmix/BingRDW/main"
+    newfname = fname + "_new.py"
+    funame = os.path.basename(__file__)
+    newfuname = funame + "_new.py"
+
+    r = requests.get(repo + "/MD5", stream = True)
+    if r.status_code == 200:
+        new_py_md5, new_fun_md5 = r.text.splitlines()
+
+    # Check current file md5
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hashlib.md5().update(chunk)
+
+    old_md5 = hashlib.md5().hexdigest()
+
+    # Compare MD5
+    if old_md5.casefold() != new_py_md5.casefold():
+        print("Found a new update! Updating...")
+        # Updating begins now.
+        r = requests.get(repo + "/BingRDW.py", stream = True)
+        # Download 1/2
+        if r.status_code == 200:
+            r.raw.decode_content = True
+            with open(newfname, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+                f.close()
+            # Compare 1/2
+            with open(newfname, "rb") as fr:
+                for chunk in iter(lambda: fr.read(4096), b""):
+                    hashlib.md5().update(chunk)
+                fr.close()
+                
+                old_md5 = hashlib.md5().hexdigest()
+                if old_md5.casefold() != new_py_md5.casefold():
+                    print("Download successful! (1/2)")
+                    # Download 2/2
+                    r = requests.get(repo + "/fun.py", stream = True)
+                    
+                    if r.status_code == 200:
+                        r.raw.decode_content = True
+                        with open(newfuname, 'wb') as f:
+                            shutil.copyfileobj(r.raw, f)
+                            f.close()
+                        # Compare 2/2
+                        with open(newfuname, "rb") as fr:
+                            for chunk in iter(lambda: fr.read(4096), b""):
+                                hashlib.md5().update(chunk)
+                            fr.close()
+
+                        old_md5 = hashlib.md5().hexdigest()
+                        if old_md5.casefold() != new_fun_md5.casefold():
+                            print("Download successful! (2/2)")
+                            print("Great! Continuing update...")
+                            os.remove(fname)
+                            os.rename(newfname, fname)
+                            print("Update successful! (1/2)")
+                            os.remove(funame)
+                            os.rename(newfuname, funame)
+                            print("Update successful! (2/2)")
+                            print("Great! Restarting....")
+                            os.execl(sys.executable, sys.executable, *sys.argv)
+                        else:
+                            print("Download failed. Skipping update...")
+                            os.remove(newfname)
+                            return
+                else:
+                    print("Download failed. Skipping update...")
+                    os.remove(newfname)
+                    return
+    else:
+        print("No update found.")
+
+    
